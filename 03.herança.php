012dae7a815acfb2bf3c620e690ddde7484f1097
Leonardo George <leonardogeorge30@gmail.com>
2025-06-28 18:22:15 -0300

    update

--- 03.herança.php
+++ 03.herança.php
@@ -2,7 +2,7 @@
 class Veiculo {
     public string $marca;
 
-    public function __construct($marca) {
+    public function __construct(string $marca) {
         $this->marca = $marca; // Atribui a marca do veículo
     }
 
@@ -15,14 +15,14 @@ class Veiculo {
 class Carro extends Veiculo {
     public $modelo; // Atributo específico da classe Carro
 
-    public function __contruct($marca, $modelo) {
+    public function __contruct(string $marca, string $modelo) {
         // Construtor da classe Carro que chama o construtor da classe pai Veiculo
         parent::__construct($marca); // Chama o construtor da classe pai
         $this->modelo =$modelo; // Atribui o modelo do carro
     }    
 }
 
-$carro = new Carro("Toyota", "Corolla"); // Cria um objeto da classe Carro
+$carro = new Veiculo("Toyota", "Corolla"); // Cria um objeto da classe Carro
 $carro->fazerBuzinar(); // Chama o método da classe pai Veiculo
 
 
